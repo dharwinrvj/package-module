4d070cd12dedb8de1a1050c406065511c09dae19
tavamaniraja <68153877+tavamaniraja@users.noreply.github.com>
2021-04-09 19:06:44 +0530

    Add files via upload

---
+++ web.php
@@ -0,0 +1,113 @@
+<html>
+ <head>
+  <meta http-equiv="Content-Type" content="text/html; charset=utf-8" />
+  <title>Webslesson Tutorial</title>
+  <script src="https://ajax.googleapis.com/ajax/libs/jquery/2.1.3/jquery.min.js"></script>
+  <script src="https://maxcdn.bootstrapcdn.com/bootstrap/3.3.6/js/bootstrap.min.js"></script>
+  <link href="https://maxcdn.bootstrapcdn.com/bootstrap/3.3.6/css/bootstrap.min.css" rel="stylesheet" />
+ </head>
+ <body>
+  <div class="container">
+   <br />
+   <div class="form-group">
+    <div class="input-group">
+     <form action="/users" method="post"><div style="margin:0;padding:0">
+  <div>
+             <input type="text" name="search_en" id="search_en" placeholder="Encounter No" size="30" class="form-control" />
+</div>
+<div>
+             <input type="text" name="search_ip" id="search_ip" placeholder="IP no " size="30" class="form-control" />
+</div>
+             <input type="text" name="search_pid" id="search_pid" placeholder="PID no " class="form-control" />
+             <input type="text" name="search_age" id="search_age" placeholder="Age " class="form-control" />
+     
+    </div>
+   </div>
+   <br />
+   <div id="result"></div>
+  </div>
+ </body>
+</html>
+
+
+<script>
+$(document).ready(function(){
+
+ load_data();
+
+ function load_data(query)
+ {
+  $.ajax({
+   url:"fetch.php",
+   method:"POST",
+   data:{query:query},
+   success:function(data)
+   {
+    $('#result').html(data);
+   }
+  });
+ }
+ $('#search_en').keyup(function(){
+  var search = $(this).val();
+  if(search != '')
+  {
+   load_data(search);
+  }
+  else
+  {
+   load_data();
+  }
+ });
+ 
+ $('#search_ip').keyup(function(){
+  var search = $(this).val();
+  if(search != '')
+  {
+   load_data(search);
+  }
+  else
+  {
+   load_data();
+  }
+ });
+
+
+ $('#search_pid').keyup(function(){
+  var search = $(this).val();
+  if(search != '')
+  {
+   load_data(search);
+  }
+  else
+  {
+   load_data();
+  }
+ });
+ 
+ $('#search_sex').keyup(function(){
+  var search = $(this).val();
+  if(search != '')
+  {
+   load_data(search);
+  }
+  else
+  {
+   load_data();
+  }
+ });
+
+
+ $('#search_age').keyup(function(){
+  var search = $(this).val();
+  if(search != '')
+  {
+   load_data(search);
+  }
+  else
+  {
+   load_data();
+  }
+ });
+
+});
+</script>
