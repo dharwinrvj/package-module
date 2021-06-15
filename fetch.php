b31b4e0a58c0164fb5c5a91c4e07b449939710bd
DharwinRVJ <dharwinruppa1422@gmail.com>
2021-06-15 21:36:23 +0530

    back-end

--- fetch.php
+++ fetch.php
@@ -1,32 +1,28 @@
 <?php
 //fetch.php
-$connect = mysqli_connect("localhost", "root", "", "patient search");
+$connect = mysqli_connect("localhost", "root", "", "velammal_hospital");
 $output = '';
-if(isset($_POST["query"]))
-{
- $search = mysqli_real_escape_string($connect, $_POST["query"]);
- $query = "
+if (isset($_POST["query"])) {
+   $search = mysqli_real_escape_string($connect, $_POST["query"]);
+   $query = "
   SELECT * FROM patient
-  WHERE encounterno LIKE '%".$search."%'
-  OR ipno LIKE '%".$search."%' 
-  OR pidno LIKE '%".$search."%' 
-  OR name LIKE '%".$search."%' 
-  OR ward LIKE '%".$search."%'
-  OR room LIKE '%".$search."%'
- OR age LIKE '%".$search."%'
- OR package LIKE '%".$search."%'
+  WHERE encounterno LIKE '%" . $search . "%'
+  OR ipno LIKE '%" . $search . "%' 
+  OR pidno LIKE '%" . $search . "%' 
+  OR name LIKE '%" . $search . "%' 
+  OR ward LIKE '%" . $search . "%'
+  OR room LIKE '%" . $search . "%'
+ OR age LIKE '%" . $search . "%'
+ OR package LIKE '%" . $search . "%'
  ";
-}
-else
-{
- $query = "
+} else {
+   $query = "
   SELECT * FROM patient ORDER BY name
  ";
 }
 $result = mysqli_query($connect, $query);
-if(mysqli_num_rows($result) > 0)
-{
- $output .= '
+if (mysqli_num_rows($result) > 0) {
+   $output .= '
   <div class="table-responsive">
    <table class="table table bordered">
     <tr>
@@ -40,26 +36,21 @@ if(mysqli_num_rows($result) > 0)
    <th>Package</th>
     </tr>
  ';
- while($row = mysqli_fetch_array($result))
- {
-  $output .= '
+   while ($row = mysqli_fetch_array($result)) {
+      $output .= '
    <tr>
-    <td>'.$row["encounterno"].'</td>
-    <td>'.$row["ipno"].'</td>
-    <td>'.$row["pidno"].'</td>
-    <td>'.$row["name"].'</td>
-    <td>'.$row["ward"].'</td>
- <td>'.$row["room"].'</td>
- <td>'.$row["age"].'</td>
- <td>'.$row["package"].'</td>
+    <td>' . $row["encounterno"] . '</td>
+    <td>' . $row["ipno"] . '</td>
+    <td>' . $row["pidno"] . '</td>
+    <td>' . $row["name"] . '</td>
+    <td>' . $row["ward"] . '</td>
+ <td>' . $row["room"] . '</td>
+ <td>' . $row["age"] . '</td>
+ <td>' . $row["package"] . '</td>
    </tr>
   ';
- }
- echo $output;
+   }
+   echo $output;
+} else {
+   echo '<p align="center">Data Not Found</p>';
 }
-else
-{
- echo 'Data Not Found';
-}
-
-?>
--- patient-search.php
+++ patient-search.php
@@ -1,16 +1,17 @@
 <html>
- <head>
-  <meta http-equiv="Content-Type" content="text/html; charset=utf-8" />
-    <link rel="stylesheet" href="https://cdn.jsdelivr.net/npm/bootstrap@4.6.0/dist/css/bootstrap.min.css"
-        integrity="sha384-B0vP5xmATw1+K9KRQjQERJvTumQW0nPEzvF6L/Z6nronJ3oUOFUFpCjEUQouq2+l" crossorigin="anonymous">
+
+<head>
+    <meta http-equiv="Content-Type" content="text/html; charset=utf-8" />
+    <link rel="stylesheet" href="https://cdn.jsdelivr.net/npm/bootstrap@4.6.0/dist/css/bootstrap.min.css" integrity="sha384-B0vP5xmATw1+K9KRQjQERJvTumQW0nPEzvF6L/Z6nronJ3oUOFUFpCjEUQouq2+l" crossorigin="anonymous">
     <link rel="stylesheet" href="./style.css">
     <title>Velammal Hospital</title>
-  <script src="https://ajax.googleapis.com/ajax/libs/jquery/2.1.3/jquery.min.js"></script>
-  <script src="https://maxcdn.bootstrapcdn.com/bootstrap/3.3.6/js/bootstrap.min.js"></script>
-  <link href="https://maxcdn.bootstrapcdn.com/bootstrap/3.3.6/css/bootstrap.min.css" rel="stylesheet" />
- </head>
- <body>
-   <div class="container-fluid my-3">
+    <script src="https://ajax.googleapis.com/ajax/libs/jquery/2.1.3/jquery.min.js"></script>
+    <script src="https://maxcdn.bootstrapcdn.com/bootstrap/3.3.6/js/bootstrap.min.js"></script>
+    <link href="https://maxcdn.bootstrapcdn.com/bootstrap/3.3.6/css/bootstrap.min.css" rel="stylesheet" />
+</head>
+
+<body>
+    <div class="container-fluid my-3">
         <div class="row">
             <div class="col-2">
                 <div class="list-group">
@@ -38,122 +39,106 @@
                         <form class="mb-2">
                             <div class="row">
                                 <div class="col">
-             <input type="text" name="search_en" id="search_en" placeholder="Encounter No" class="form-control" />
-</div>
-<div class="col">
-             <input type="text" name="search_ip" id="search_ip" placeholder="IP no "  class="form-control" />
-</div>
-<div class="col">
-             <input type="text" name="search_pid" id="search_pid" placeholder="PID no " class="form-control" />
-</div>
-<div class="col">
-           <input type="date" class="form-control" placeholder="Date">
-</div>
-<div class="col">
- <select class="form-control" id="exampleFormControlSelect1">
+                                    <input type="text" name="search_en" id="search_en" placeholder="Encounter No" class="form-control" />
+                                </div>
+                                <div class="col">
+                                    <input type="text" name="search_ip" id="search_ip" placeholder="IP no " class="form-control" />
+                                </div>
+                                <div class="col">
+                                    <input type="text" name="search_pid" id="search_pid" placeholder="PID no " class="form-control" />
+                                </div>
+                                <div class="col">
+                                    <input type="date" class="form-control" placeholder="Date">
+                                </div>
+                                <div class="col">
+                                    <select class="form-control" id="exampleFormControlSelect1">
                                         <option>Gender</option>
                                         <option>Male</option>
                                         <option>Female</option>
                                         <option>Transgender</option>
                                     </select>
-</div>
-<div class="col">
-             <input type="text" name="search_age" id="search_age" placeholder="Age " class="form-control" />
-</div>
-<div class="col">
+                                </div>
+                                <div class="col">
+                                    <input type="text" name="search_age" id="search_age" placeholder="Age " class="form-control" />
+                                </div>
+                                <div class="col">
                                     <input type="submit" class="btn btn-secondary" value="Submit">
                                 </div>
-    </div>
-  </form>
-</div>
-</div>
+                            </div>
+                        </form>
+                    </div>
+                </div>
 
-  <div class="form-group">
-                               <div id="result"></div>
-                        </div>
+                <div class="form-group">
+                    <div id="result"></div>
+                </div>
 
- </body>
-</html>
-
-
+</body>
 <script>
-$(document).ready(function(){
+    $(document).ready(function() {
 
- load_data();
+        load_data();
 
- function load_data(query)
- {
-  $.ajax({
-   url:"fetch.php",
-   method:"POST",
-   data:{query:query},
-   success:function(data)
-   {
-    $('#result').html(data);
-   }
-  });
- }
- $('#search_en').keyup(function(){
-  var search = $(this).val();
-  if(search != '')
-  {
-   load_data(search);
-  }
-  else
-  {
-   load_data();
-  }
- });
- 
- $('#search_ip').keyup(function(){
-  var search = $(this).val();
-  if(search != '')
-  {
-   load_data(search);
-  }
-  else
-  {
-   load_data();
-  }
- });
+        function load_data(query) {
+            $.ajax({
+                url: "fetch.php",
+                method: "POST",
+                data: {
+                    query: query
+                },
+                success: function(data) {
+                    $('#result').html(data);
+                }
+            });
+        }
+        $('#search_en').keyup(function() {
+            var search = $(this).val();
+            if (search != '') {
+                load_data(search);
+            } else {
+                load_data();
+            }
+        });
+
+        $('#search_ip').keyup(function() {
+            var search = $(this).val();
+            if (search != '') {
+                load_data(search);
+            } else {
+                load_data();
+            }
+        });
 
 
- $('#search_pid').keyup(function(){
-  var search = $(this).val();
-  if(search != '')
-  {
-   load_data(search);
-  }
-  else
-  {
-   load_data();
-  }
- });
- 
- $('#search_sex').keyup(function(){
-  var search = $(this).val();
-  if(search != '')
-  {
-   load_data(search);
-  }
-  else
-  {
-   load_data();
-  }
- });
+        $('#search_pid').keyup(function() {
+            var search = $(this).val();
+            if (search != '') {
+                load_data(search);
+            } else {
+                load_data();
+            }
+        });
+
+        $('#search_sex').keyup(function() {
+            var search = $(this).val();
+            if (search != '') {
+                load_data(search);
+            } else {
+                load_data();
+            }
+        });
 
 
- $('#search_age').keyup(function(){
-  var search = $(this).val();
-  if(search != '')
-  {
-   load_data(search);
-  }
-  else
-  {
-   load_data();
-  }
- });
+        $('#search_age').keyup(function() {
+            var search = $(this).val();
+            if (search != '') {
+                load_data(search);
+            } else {
+                load_data();
+            }
+        });
 
-});
+    });
 </script>
+
+</html>
